ac94af802fc1e39dab906140d6d38eeee8f5185f
Noel Mace <contact@noelmace.com>
2020-05-24 23:15:55 +0200

    refactor: theme color unset by default

--- darkmode.js
+++ darkmode.js
@@ -2,17 +2,20 @@
   const darkModeToggle = document.querySelector('dark-mode-toggle');
   // const manifests = document.querySelectorAll('link[rel="manifest"]');
   const themeColor = document.querySelector('meta[name="theme-color"]');
+  const msTitleColor = document.querySelector('msapplication-TileColor');
   const linkCards = document.querySelectorAll('link-card[src][data-src-light][data-src-dark]');
 
   const toggleTheme = (e) => {
     const darkModeOn = e.detail.colorScheme === 'dark' ? true : false;
+    const primaryColor = darkModeOn ? 'hsl(0,0%,20%)' : '#24292e';
     // manifests.forEach((link) => {
     //   link.href = darkModeOn ? link.dataset.hrefDark : link.dataset.hrefLight;
     // });
     linkCards.forEach((link) => {
       link.src = darkModeOn ? link.dataset.srcDark : link.dataset.srcLight;
     });
-    themeColor.content = darkModeOn ? 'hsl(0,0%,20%)' : '#24292e';
+    themeColor.content = primaryColor;
+    msTitleColor.content = primaryColor;
   };
   document.addEventListener('colorschemechange', toggleTheme);
   toggleTheme({detail: {colorScheme: darkModeToggle.mode}});
