47942c43b68b6ffb975e2549a5378acd71579095
Noel Mace <contact@noelmace.com>
2020-05-25 12:19:34 +0200

    feat: set color scheme via get param

--- darkmode.js
+++ darkmode.js
@@ -1,14 +1,21 @@
-(() => {
-  const darkModeToggle = document.querySelector('dark-mode-toggle');
-  const themeColor = document.querySelector('meta[name="theme-color"]');
-  const msTitleColor = document.querySelector('meta[name="msapplication-TileColor"]');
+import "https://unpkg.com/dark-mode-toggle";
 
-  const toggleTheme = (e) => {
-    const darkModeOn = e.detail.colorScheme === 'dark' ? true : false;
-    const primaryColor = darkModeOn ? 'hsl(0,0%,20%)' : '#24292e';
-    themeColor.content = primaryColor;
-    msTitleColor.content = primaryColor;
-  };
-  document.addEventListener('colorschemechange', toggleTheme);
-  toggleTheme({detail: {colorScheme: darkModeToggle.mode}});
-})();
+const darkModeToggle = document.querySelector('dark-mode-toggle');
+const themeColor = document.querySelector('meta[name="theme-color"]');
+const msTitleColor = document.querySelector('meta[name="msapplication-TileColor"]');
+
+const toggleTheme = (e) => {
+  const darkModeOn = e.detail.colorScheme === 'dark' ? true : false;
+  const primaryColor = darkModeOn ? 'hsl(0,0%,20%)' : '#24292e';
+  themeColor.content = primaryColor;
+  msTitleColor.content = primaryColor;
+};
+
+let searchParam = (new URL(document.location)).searchParams.get('theme');
+if (searchParam === 'dark' || searchParam === 'light') {
+  console.log(darkModeToggle);
+  darkModeToggle.setAttribute('mode', searchParam);
+}
+
+document.addEventListener('colorschemechange', toggleTheme);
+toggleTheme({detail: {colorScheme: searchParam || darkModeToggle.mode}});
